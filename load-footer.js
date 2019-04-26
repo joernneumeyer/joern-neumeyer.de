33dca7e89edec82426ef5c326e8449568f85aeca
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-04-26 08:57:08 +0200

    removed separate loading scripts

--- load-footer.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var footerElement = document.getElementsByTagName('footer')[0];
-  fetch('footer.html').then(function(response) {
-    return response.text();
-  }).then((html) => {
-    footerElement.innerHTML = html;
-  });
-})();
--- load-header.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var headerElement = document.getElementsByTagName('header')[0];
-  fetch('header.html').then(function(response) {
-    return response.text();
-  }).then((html) => {
-    headerElement.innerHTML = html;
-  });
-})();
