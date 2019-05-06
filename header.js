739d97f35a73ba7448843c52d70b2f550c44a9ef
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-05-06 20:08:13 +0200

    changed nav rendering; nav items are now fetched dynamically

--- header.js
+++ header.js
@@ -19,31 +19,30 @@
 
 document.addEventListener('DOMContentLoaded', function(){
   var headerList = document.getElementById('header-list');
-  var pages = ['home', 'articles', 'contact', 'copyright'];
   var mainElement = document.getElementsByTagName('main')[0];
   var initialLoad = true;
 
   var clickHandlerFactory = function(page) {
     return function() {
-      if (page === getPageFragment() && !initialLoad) return;
+      if (page.code === getPageFragment() && !initialLoad) return;
       document.getElementById('nav-link-' + getPageFragment()).classList.remove('is-selected');
-      document.getElementById('nav-link-' + page).classList.add('is-selected');
-      setPageFragment(page);
+      document.getElementById('nav-link-' + page.code).classList.add('is-selected');
+      setPageFragment(page.code);
       setPageTitle(this.innerText + ' - J&ouml;rn Neumeyer');
-      fetch('pages/' + page + '.html').then(function(response) {
+      fetch('pages/' + page.code + '.html').then(function(response) {
         return response.text();
       }).then(function(html) {
         mainElement.innerHTML = html;
-        fetch('pages/' + page + '.js').then(function(response) {
+        fetch('pages/' + page.code + '.js').then(function(response) {
           if (response.status === 404) {
             throw undefined;
           }
           return response.text();
         }).then(eval).catch(function(error){
           if (error === undefined) {
-            console.info('did not find a script for page ' + page);
+            console.info('did not find a script for page ' + page.code);
           } else {
-            console.error('Error while loading script for "' + page + '"');
+            console.error('Error while loading script for "' + page.code + '"');
             throw error;
           }
         });
@@ -51,14 +50,13 @@ document.addEventListener('DOMContentLoaded', function(){
     };
   };
 
-  var mapPageTitleToLink = function(page) {
-    var pageParts = page.split('-');
+  var mapPageInfoToLink = function(page) {
     var anchor = document.createElement('a');
-    anchor.innerText = pageParts.map(upperCaseFirst).join('');
+    anchor.innerText = page.title;
     anchor.classList.add('tabs-item');
     anchor.classList.add('clickable');
     anchor.classList.add('col-sm-12');
-    anchor.id = 'nav-link-' + page;
+    anchor.id = 'nav-link-' + page.code;
     anchor.addEventListener('click', clickHandlerFactory(page));
     return anchor;
   };
@@ -69,19 +67,25 @@ document.addEventListener('DOMContentLoaded', function(){
     return span;
   };
 
-  pages
-    .map(mapPageTitleToLink)
-    .forEach(function(link, i) {
-      var linkWrapper = createElementWithAttributes('div', {
-        className: 'col-sm-3'
-      });
-      linkWrapper.appendChild(link);
-      headerList.appendChild(linkWrapper);
+  fetch('/pages/pages.json')
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(pages) {
+      pages
+        .map(mapPageInfoToLink)
+        .forEach(function(link, i) {
+          var linkWrapper = createElementWithAttributes('div', {
+            className: 'col-sm-3'
+          });
+          linkWrapper.appendChild(link);
+          headerList.appendChild(linkWrapper);
+        });
+
+      setPageFragment(getPageFragment() || 'home');
+
+      var linkId = 'nav-link-' + (getPageFragment() || 'home');
+      document.getElementById(linkId).click();
+      initialLoad = false;
     });
-
-  setPageFragment(getPageFragment() || 'home');
-
-  var linkId = 'nav-link-' + (getPageFragment() || 'home');
-  document.getElementById(linkId).click();
-  initialLoad = false;
 });
