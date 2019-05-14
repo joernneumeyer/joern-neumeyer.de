e813b136c2ead4e71f9fd8e38703d43d7956fd6d
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-05-14 23:02:50 +0200

    updated comment

--- articles/c-pointer-arithmetic/example-stack.c
+++ articles/c-pointer-arithmetic/example-stack.c
@@ -6,7 +6,7 @@ int main() {
   a = 5;
   c = 6;
   printf("a@%d\r\nb@%d\r\nc@%d\r\n", (int)&a, (int)&b, (int)&c);
-  // prints memory addresses like:
+  // prints memory addresses like the following (depending on architecture):
   // a@-351270344
   // b@-351270352
   // c@-351270348
